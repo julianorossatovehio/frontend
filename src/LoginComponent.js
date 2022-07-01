f55efe52c98eec3853c77381d3fc3c4049981790
julianorossatovehio <64209448+julianorossatovehio@users.noreply.github.com>
2022-07-01 08:11:20 -0300

    Update LoginComponent.js

--- src/LoginComponent.js
+++ src/LoginComponent.js
@@ -20,7 +20,7 @@ export default class LoginComponent extends React.Component{
       }
     
       handleSubmit(event) {
-        var url = 'http://44.202.255.149:8080/api-token-auth/';
+        var url = 'http://54.221.167.221:8080/api-token-auth/';
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
